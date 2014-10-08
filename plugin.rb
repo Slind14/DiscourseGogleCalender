d73f349e3fe2479573d73dcdff96a588bfb41500
Slind14 <slind014@gmail.com>
2014-10-08 23:45:15 +0200

    ..

--- plugin.rb
+++ plugin.rb
@@ -4,8 +4,8 @@ module Onebox
   module Engine
     class GoogleCalendarOnebox
       include Engine
-      REGEX = /^https?:\/\/(?:www\.)?google\.com\/calendar\/embed\?.+/
-      matches_regexp REGEX
+      
+      matches_regexp(/^https?:\/\/(?:www\.)?google\.com\/calendar\/embed\?.+/)
 
       def to_html
         "<iframe src='#{@url}' style='border-width:0' frameborder='0' scrolling='no' width='100%'></iframe>"
