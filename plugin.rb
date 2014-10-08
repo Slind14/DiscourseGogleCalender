c83d1be445c659bdd51dc969bc669180d937eb53
Slind14 <slind014@gmail.com>
2014-10-08 23:20:09 +0200

    initial commit

---
+++ plugin.rb
@@ -0,0 +1,12 @@
+module Onebox
+  module Engine
+    class GoogleCalendarOnebox
+      include Engine
+      matches_regexp /^https?:\/\/(?:www\.)?google\.com\/calendar\/embed\?.+/
+
+      def to_html
+        "<iframe src='#{@url}' style='border-width:0' frameborder='0' scrolling='no' width='100%'></iframe>"
+      end
+    end
+  end
+end
